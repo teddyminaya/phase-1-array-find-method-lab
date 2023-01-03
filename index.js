a5289583a996d54fae4c2322ba427d5cd20acfd1
Teddy Minaya <teddyminaya@gmail.com>
2023-01-03 18:08:55 -0500

    commit tree 5ea335340c1f40710e1e8ff099a2d66fc442f111

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    for (const game of record) {
+      if (game.result === "W") {
+        return game.year;
+      }
+    }
+    return undefined;
+  }
